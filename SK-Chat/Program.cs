b5dad86f4e121ce35ce1bb76c1655f6386b363d7
gsw <axzxs2001@163.com>
2025-10-12 21:22:52 +0900

    1、添加SK两个项目

---
+++ SK-Chat/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.TextToImage;
+using Microsoft.SemanticKernel.TextToAudio;
+using Microsoft.SemanticKernel.AudioToText;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+var arr = File.ReadLines("C://gpt/azure_key.txt").ToArray();
+var endpoint = arr[1];
+var deploymentName = arr[0];
+var key = arr[2];
+
+app.MapPost("/chat", async (PromptMessage chatPrompt) =>
+{
+    var kernelBuilder = Kernel.CreateBuilder();
+    kernelBuilder.AddAzureOpenAIChatCompletion(
+        deploymentName: deploymentName,
+        endpoint: endpoint,
+        apiKey: key);    
+    var kernel = kernelBuilder.Build();
+    var chatHistory = new ChatHistory("你是一个乐于助人的助手。");
+    chatHistory.AddUserMessage(chatPrompt.Input);
+    var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+    var reply = await chatCompletionService.GetChatMessageContentAsync(chatHistory);
+    return Results.Ok(reply.Content);
+})
+.WithName("chat");
+
+app.Run();
+public class PromptMessage
+{
+    public string Input { get; set; }
+}
---
+++ SK-ImageToText/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+var arr = File.ReadLines("C://gpt/azure_key.txt").ToArray();
+var endpoint = arr[1];
+var deploymentName = arr[0];
+var key = arr[2];
+
+
+var kernelBuilder = Kernel.CreateBuilder();
+kernelBuilder.AddAzureOpenAIChatCompletion(
+    deploymentName: deploymentName,
+    endpoint: endpoint,
+    apiKey: key);
+var kernel = kernelBuilder.Build();
+var chatHistory = new ChatHistory("你是一个图像解释助手。");
+
+
+var imageFilePath = "mcp-dev-use.jpg"; 
+var imageBytes = await File.ReadAllBytesAsync(imageFilePath);
+var imageMemory = new ReadOnlyMemory<byte>(imageBytes);
+
+chatHistory.AddUserMessage(new ChatMessageContentItemCollection
+{
+    new TextContent("请说明漫画的寓意是什么"),
+    new ImageContent(imageMemory, "image/jpeg") 
+});
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+Console.WriteLine("AI 响应:");
+await foreach (var message in chatCompletionService.GetStreamingChatMessageContentsAsync(chatHistory))
+{
+    Console.Write(message.Content);
+}
+Console.ReadLine();
