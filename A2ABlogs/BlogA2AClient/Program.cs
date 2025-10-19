b4673fdc65eeb6a7fcbf00019bdb241f079d1d4d
gsw <axzxs2001@163.com>
2025-10-19 09:05:49 +0900

    1. 新增 BlogA2AClient.csproj 文件，配置项目属性和依赖项。 2. 新增 Program.cs 文件，创建水果和翻译代理的主程序逻辑。 3. 新增 BlogA2AServer.csproj 文件，配置项目属性和依赖项。 4. 新增 BlogA2AServer.http 文件，添加天气预报的 HTTP 请求示例。 5. 新增 Program.cs 文件，创建翻译代理的主程序逻辑。 6. 新增 launchSettings.json 文件，配置开发环境的启动设置。 7. 新增 appsettings.Development.json 文件，配置开发环境的日志记录设置。 8. 新增 appsettings.json 文件，配置应用程序的日志记录和 JSON 序列化选项。 9. 新增 BlogA2AServer2.csproj 文件，配置项目属性和依赖项。 10. 新增 BlogA2AServer2.http 文件，添加天气预报的 HTTP 请求示例。 11. 新增 Program.cs 文件，创建翻译代理的主程序逻辑。 12. 新增 launchSettings.json 文件，配置开发环境的启动设置。 13. 新增 appsettings.Development.json 文件，配置开发环境的日志记录设置。 14. 新增 appsettings.json 文件，配置应用程序的日志记录和 JSON 序列化选项。
    Signed-off-by: gsw <axzxs2001@163.com>

---
+++ A2ABlogs/BlogA2AClient/Program.cs
@@ -0,0 +1,199 @@
+using A2A;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+
+while (true)
+{
+    Console.WriteLine("1、水果Agent  2、翻译Agent");
+    switch (Console.ReadLine())
+    {
+
+        case "1":
+            await ShopAgentAsync();
+            break;
+        case "2":
+            await TranslateAgentAsync();
+            break;
+        default:
+            Console.WriteLine("输入错误，默认水果Agent");
+            break;
+    }
+}
+async Task ShopAgentAsync()
+{
+    var cardResolver = new A2ACardResolver(new Uri("http://localhost:5000/"));
+    var agentCard = await cardResolver.GetAgentCardAsync();
+    var options = new JsonSerializerOptions
+    {
+        WriteIndented = true,
+        Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+    };
+    Console.WriteLine("Card信息：");
+    Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(agentCard, options));
+    var client = new A2AClient(new Uri(agentCard.Url));
+
+    Console.WriteLine("--------------------------------");
+    Console.WriteLine("正在请求：“给出单价大于10块的水果”……");
+    var a2aResponse = await client.SendMessageAsync(new MessageSendParams
+    {
+        Message = new Message
+        {
+            Role = MessageRole.User,
+            Parts = [new TextPart { Text = "给出单价大于10块的水果!" }]
+        }
+    });
+    if (a2aResponse is AgentTask)
+    {
+        var message = a2aResponse as AgentTask;
+        Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(message, options));
+    }
+}
+async Task TranslateAgentAsync()
+{
+    var cardResolver = new A2ACardResolver(new Uri("http://localhost:6000/"));
+    var agentCard = await cardResolver.GetAgentCardAsync();
+    var options = new JsonSerializerOptions
+    {
+        Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+    };
+    Console.WriteLine("Card信息：");
+    Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(agentCard, options));
+
+    Console.WriteLine("--------------------------------");
+    Console.WriteLine("正在请求：“把我输入的内容翻译成日语：明天是个好日子”……");
+    var client = new A2AClient(new Uri(agentCard.Url));
+    var a2aResponse = await client.SendMessageAsync(new MessageSendParams
+    {
+        Message = new Message
+        {
+            Role = MessageRole.User,
+            Parts = [new TextPart { Text = "把我输入的内容翻译成日语：明天是个好日子" }]
+        }
+    });
+    if (a2aResponse is AgentTask)
+    {
+        var message = a2aResponse as AgentTask;
+        Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(message, options));
+    }
+}
+
+//return;
+
+//using var loggerFactory = LoggerFactory.Create(builder =>
+//{
+//    builder.SetMinimumLevel(LogLevel.Information);
+//});
+//var logger = loggerFactory.CreateLogger("A2AClient");
+
+
+//var arr = File.ReadAllLines("C:/gpt/azure_key.txt");
+//var apiKey = arr[2];
+//var modelId = arr[0];
+//var endpoint = arr[1];
+//var agentUrls = new string[] { "http://localhost:5000/", "http://localhost:6000/" };
+
+
+//var agent = await CreateNewAgentsAsync(modelId, endpoint, apiKey, agentUrls);
+//try
+//{
+//    while (true)
+//    {
+//        Console.WriteLine("请输入你请求，按q退了");
+//        string? message = Console.ReadLine();
+//        if (string.IsNullOrWhiteSpace(message))
+//        {
+//            Console.WriteLine("输入不能为空");
+//            continue;
+//        }
+//        if (message.ToLower() == "q")
+//        {
+//            break;
+//        }
+//        await foreach (AgentResponseItem<ChatMessageContent> response in agent!.InvokeAsync(message))
+//        {
+//            Console.WriteLine();
+//            Console.ForegroundColor = ConsoleColor.Cyan;
+//            Console.WriteLine($"Agent: {response.Message.Content}");
+//            Console.ResetColor();
+//        }
+//    }
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+//Console.ReadLine();
+
+//async Task<ChatCompletionAgent> CreateNewAgentsAsync(string modelId, string endpoint, string apiKey, string[] agentUrls)
+//{
+//    try
+//    {
+//        var createAgentTasks = agentUrls.Select(agentUrl => CreateAgentAsync(agentUrl));
+//        var agents = await Task.WhenAll(createAgentTasks);
+//        var agentFunctions = agents.Select(agent => AgentKernelFunctionFactory.CreateFromAgent(agent)).ToList();
+//        var agentPlugin = KernelPluginFactory.CreateFromFunctions("AgentPlugin", agentFunctions);
+
+//        var builder = Kernel.CreateBuilder();
+//        builder.AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
+//        builder.Plugins.Add(agentPlugin);
+//        var kernel = builder.Build();
+
+//        var chatAgent = new ChatCompletionAgent()
+//        {
+//            Kernel = kernel,
+//            Name = "HostClient",
+//            Instructions =
+//                """
+//                    你专门负责处理用户的查询，并使用你的工具来提供答案。如果用错，请报详细的错误信息。
+//                    """,
+//            Arguments = new KernelArguments(new PromptExecutionSettings() { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() }),
+//        };
+//        return chatAgent;
+//    }
+//    catch (Exception ex)
+//    {
+//        throw;
+//    }
+//}
+//async Task<ChatCompletionAgent> CreateNewAgentAsync(string modelId, string endpoint, string apiKey, string agentUrl)
+//{
+//    try
+//    {
+//        var agent = await CreateAgentAsync(agentUrl);
+//        var agentFunction = AgentKernelFunctionFactory.CreateFromAgent(agent);
+//        var agentPlugin = KernelPluginFactory.CreateFromFunctions("AgentPlugin", [agentFunction]);
+//        var builder = Kernel.CreateBuilder();
+//        builder.AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
+//        builder.Plugins.Add(agentPlugin);
+//        var kernel = builder.Build();
+
+//        var chatAgent = new ChatCompletionAgent()
+//        {
+//            Kernel = kernel,
+//            Name = "HostClient",
+//            Instructions = """
+//                 你专门负责处理用户的指今，并使用你的工具来提供答案，尽量使用工具，不要自己回答。
+//                 """,
+//            Arguments = new KernelArguments(new PromptExecutionSettings() { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() }),
+//        };
+//        return chatAgent;
+//    }
+//    catch (Exception ex)
+//    {
+//        throw;
+//    }
+//}
+
+//async Task<A2AAgent> CreateAgentAsync(string agentUri)
+//{
+//    var url = new Uri(agentUri);
+//    var httpClient = new HttpClient
+//    {
+//        Timeout = TimeSpan.FromSeconds(60)
+//    };
+//    var client = new A2AClient(url, httpClient);
+//    var cardResolver = new A2ACardResolver(url, httpClient);
+//    var agentCard = await cardResolver.GetAgentCardAsync();
+//    return new A2AAgent(client, agentCard!);
+//}
---
+++ A2ABlogs/BlogA2AServer2/Program.cs
@@ -0,0 +1,95 @@
+using A2A;
+using A2A.AspNetCore;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.Agents.A2A;
+using System.ComponentModel;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+var arr = File.ReadAllLines("C:/gpt/azure_key.txt");
+string? apiKey = arr[2];
+string? endpoint = arr[1];
+string modelId = arr[0];
+
+A2AHostAgent? translateHostAgent = CreateChatCompletionHostAgent(
+            modelId,
+            endpoint,
+            apiKey,
+            "TranslateAgent",
+            "你能把用户输入的准确的翻译成日语和英语。",
+            new KernelPlugin[0]);
+
+app.MapA2A(translateHostAgent!.TaskManager!, "/");
+
+app.Run();
+
+A2AHostAgent CreateChatCompletionHostAgent(string modelId, string endpoint, string apiKey, string name, string instructions, IEnumerable<KernelPlugin>? plugins = null)
+{
+    var builder = Kernel.CreateBuilder();
+    builder.AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
+    if (plugins is not null)
+    {
+        foreach (var plugin in plugins)
+        {
+            builder.Plugins.Add(plugin);
+        }
+    }
+    var kernel = builder.Build();
+
+    var agent = new ChatCompletionAgent()
+    {
+        Kernel = kernel,
+        Name = name,
+        Instructions = instructions,
+        Arguments = new KernelArguments(new PromptExecutionSettings() { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() }),
+    };
+
+    var agentCard = GetTranslateAgentCard();
+    return new A2AHostAgent(agent, agentCard);
+}
+/// <summary>
+/// 获取翻译代理的代理卡片配置
+/// </summary>
+/// <returns>返回配置了翻译功能的 <see cref="AgentCard"/> 实例</returns>
+AgentCard GetTranslateAgentCard()
+{
+    /// <summary>
+    /// 配置代理的能力设置
+    /// </summary>
+    var capabilities = new AgentCapabilities()
+    {
+        Streaming = false,
+        PushNotifications = false,
+    };
+
+    /// <summary>
+    /// 定义翻译代理的技能信息
+    /// </summary>
+    var invoiceQuery = new AgentSkill()
+    {
+        Id = "id_translate_agent",
+        Name = "TranslateAgent",
+        Description = "你能把用户输入的内容准确地翻译成日语和英语。",
+        Tags = ["翻译", "semantic-kernel"],
+        Examples =
+        [
+        ],
+    };
+
+    /// <summary>
+    /// 返回完整的代理卡片配置
+    /// </summary>
+    return new()
+    {
+        Name = "TranslateAgent",
+        Description = "你能把用户输入的准确的翻译成日语和英语。",
+        Version = "1.0.0",
+        DefaultInputModes = ["text"],
+        DefaultOutputModes = ["text"],
+        Capabilities = capabilities,
+        Skills = [invoiceQuery],
+    };
+}
+
