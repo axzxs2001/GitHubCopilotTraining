b3bcd0342fb3c2b30a6b0ad4eb059bed961706c3
gsw <axzxs2001@163.com>
2025-10-22 16:25:43 +0900

    1. 更新 InlineChatDemo.cs，添加 SHA256 哈希函数实现。 2. 更新 Program.cs，移除乘法和平方根函数的实现。
    Signed-off-by: gsw <axzxs2001@163.com>

--- ChatDemo/InlineChatDemo.cs
+++ ChatDemo/InlineChatDemo.cs
@@ -1,13 +1,23 @@
 
 
 public class InlineChatDemo
-{   
+{
     public string HashString(string input)
     {
         using (var md5 = System.Security.Cryptography.MD5.Create())
         {
             var inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
             var hashBytes = md5.ComputeHash(inputBytes);
+            return System.BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
+        }
+    }
+
+    public string HashStringSHA256(string input)
+    {
+        using (var sha256 = System.Security.Cryptography.SHA256.Create())
+        {
+            var inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
+            var hashBytes = sha256.ComputeHash(inputBytes);
             return System.BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
         }
     }
--- ChatDemo/Program.cs
+++ ChatDemo/Program.cs
@@ -20,23 +20,3 @@ static int Subtract(int a, int b)
 {
     return a - b;
 }
-/// <summary>
-/// 两个整数相乘 / 2つの整数を乗算する
-/// </summary>
-/// <param name="a">第一个乘数 / 最初の乗数</param>
-/// <param name="b">第二个乘数 / 2番目の乗数</param>
-/// <returns>返回两数之积 / 2つの数の積を返す</returns>
-static int Multiply(int a, int b)
-{
-    return a * b;
-}
-/// <summary>
-/// 计算一个数的平方根 / 数値の平方根を計算する
-/// </summary>
-/// <param name="a">需要计算平方根的数值 / 平方根を計算する数値</param>
-/// <returns>返回平方根值 / 平方根の値を返す</returns>
-static double sqrt(double a)
-{
-    return Math.Sqrt(a);
-}
-
